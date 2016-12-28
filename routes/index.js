d9c68830eb51cac885a2df5b2318d8ce569fd064
Vikram Kalta <kaltarulingthesea@live.com>
2016-12-28 06:21:22 +0000

    add products

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,37 @@ var router = express.Router();
 var Cart = require("../models/cart")
 var Product = require("../models/product");
 var Order = require("../models/order");
+var mongoose = require("mongoose");
 
 router.get("/", function(req, res, next){
     var successMsg = req.flash("success")[0];
+    var products = [
+    new Product({
+   imagePath: "https://i.ytimg.com/vi/b4ySfqwG0hs/maxresdefault.jpg",
+   title: "Gothic videogame",
+   description: "Awesome game",
+   price: 10
+}),
+   new Product({
+   imagePath: "http://files.all-free-download.com//downloadfiles/wallpapers/1680_1050/mafia_ii_wallpaper_mafia_2_games_3171.jpg",
+   title: "Super Mario",
+   description: "Awesome game",
+   price: 10
+}),
+   new Product({
+   imagePath: "http://www.desktopwallpaperhd.net/wallpapers/18/4/game-design-beautiful-background-wallpaper-games-185860.jpg",
+   title: "San Andreas",
+   description: "Awesome game",
+   price: 10
+}),
+   new Product({
+   imagePath: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcT84WgPF9tULJUbCLvsC3PSCBQJRgxI1giQqwaiBfgNs43g7u84",
+   title: "Max Payne",
+   description: "Awesome game",
+   price: 10
+}),
+];
+
     Product.find(function(err, docs){
         if(err){
             console.log(err)
